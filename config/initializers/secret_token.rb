d0463e3f481ff3c49740b6bd1b5a6dabb6b13ce7
Andreas Fornell <afornell@gmail.com>
2012-01-05 22:00:19 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Andreasfornell::Application.config.secret_token = 'c7dabf63629b1b4c9b3a637d2c75004e49bc72ad3bffd292b27d99ef552991e0fa245e0f263b2e0dae485fbf72a2bbe5bf0b75ecff30df2546158cead550b1b5'
